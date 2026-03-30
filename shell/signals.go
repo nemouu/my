bcd219971675fa23acd1ff047c6641cc8d50abbe
nemouu <soerenfjallback@gmail.com>
2026-03-30 13:17:16 +0200

    created the first 3 handler implementations; SIGINT, SIGTSTP, SIGCHLD

--- shell/signals.go
+++ shell/signals.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"errors"
 	"os"
 	"os/signal"
 	"syscall"
@@ -36,18 +37,51 @@ func initSignalHandlers() {
 
 // handleSIGINT handles Ctrl+C (SIGINT).
 // Should forward signal to the current foreground job if one exists.
-// TODO: Milestone 1 - forward to foreground process group
 func handleSIGINT() {
+	job, err := getForegroundJob()
+	if err != nil {
+		return // no foreground job, nothing to do
+	}
+	syscall.Kill(-job.pid, syscall.SIGINT)
 }
 
 // handleSIGTSTP handles Ctrl+Z (SIGTSTP).
 // Should stop the current foreground job if one exists.
-// TODO: Milestone 1 - forward to foreground process group
 func handleSIGTSTP() {
+	job, err := getForegroundJob()
+	if err != nil {
+		return // no foreground job, nothing to do
+	}
+	syscall.Kill(-job.pid, syscall.SIGTSTP)
+	job.state = ST
 }
 
 // handleSIGCHLD handles child process state changes (SIGCHLD).
 // Responsible for reaping zombie processes.
-// TODO: Milestone 1 - call waitpid to reap finished children
 func handleSIGCHLD() {
+	for {
+		var status syscall.WaitStatus
+		pid, err := syscall.Wait4(-1, &status, syscall.WNOHANG, nil)
+		if err != nil || pid <= 0 {
+			break
+		}
+		if status.Stopped() {
+			job, err := getJobByPid(pid)
+			if err == nil {
+				job.state = ST
+			}
+		} else {
+			deleteJob(pid)
+		}
+	}
+}
+
+// Helper function
+func getForegroundJob() (*Job, error) {
+	for i := range jobs {
+		if jobs[i].state == FG {
+			return &jobs[i], nil
+		}
+	}
+	return nil, errors.New("no foreground job found")
 }
